29745e5d4aef07c094af9cd689ebb9b2d796cec8
emrhky <emrhky.ek@gmail.com>
2026-01-12 03:20:21 +0300

    Update index.js

--- app/index.js
+++ app/index.js
@@ -1,15 +1,39 @@
 import document from "document";
 import * as messaging from "messaging";
 
-const txt0 = document.getElementById("text-0");
+// 5 adet not alanı (Daha fazla istersen SVG'ye de eklemelisin)
+const noteTexts = [
+  document.getElementById("text-0"),
+  document.getElementById("text-1"),
+  document.getElementById("text-2"),
+  document.getElementById("text-3"),
+  document.getElementById("text-4")
+];
 
-messaging.peerSocket.onopen = () => {
-  txt0.text = "Baglanti OK. Not Yaziniz...";
-};
+const rects = [
+  document.getElementById("rect-0"),
+  document.getElementById("rect-1"),
+  document.getElementById("rect-2"),
+  document.getElementById("rect-3"),
+  document.getElementById("rect-4")
+];
 
 messaging.peerSocket.onmessage = (evt) => {
-  // Gelen veri ne olursa olsun ekrana yaz
-  if (evt.data) {
-    txt0.text = evt.data.substring(0, 30); // Ekrana sığması için kısıtla
+  const incomingNotes = evt.data; // Bu bir dizi []
+  
+  if (Array.isArray(incomingNotes)) {
+    for (let i = 0; i < noteTexts.length; i++) {
+      if (incomingNotes[i]) {
+        noteTexts[i].text = incomingNotes[i].substring(0, 25);
+        rects[i].style.display = "inline";
+      } else {
+        noteTexts[i].text = "";
+        rects[i].style.display = "none";
+      }
+    }
   }
+};
+
+messaging.peerSocket.onopen = () => {
+  console.log("Bağlantı aktif");
 };
