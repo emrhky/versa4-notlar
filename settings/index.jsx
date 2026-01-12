7b1b4228753bfae06ce2fa02e3189f638e9b9498
emrhky <emrhky.ek@gmail.com>
2026-01-12 03:11:33 +0300

    Update index.jsx

--- settings/index.jsx
+++ settings/index.jsx
@@ -1,12 +1,10 @@
-registerSettingsPage((props) => {
-  const rows = [];
-  for (let i = 0; i < 10; i++) {
-    rows.push(
-      <Section title={`Not ${i+1}`}>
-        <TextInput label="Baslik" settingsKey={`title_${i}`} />
-        <TextInput label="Icerik" settingsKey={`content_${i}`} />
-      </Section>
-    );
-  }
-  return <Page>{rows}</Page>;
-});
+registerSettingsPage((props) => (
+  <Page>
+    <Section title="Not Girin">
+      <TextInput
+        label="Notunuz"
+        settingsKey="not_icerigi"
+      />
+    </Section>
+  </Page>
+));
