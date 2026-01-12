d83f23d9552030794815899f6b4b969507d937a9
emrhky <emrhky.ek@gmail.com>
2026-01-12 09:29:11 +0300

    Update index.js

--- companion/index.js
+++ companion/index.js
@@ -6,17 +6,11 @@ function sendData() {
     const data = settingsStorage.getItem("notes_list");
     if (data) {
       const raw = JSON.parse(data);
-      const clean = raw.map(item => {
-        let full = typeof item === 'object' ? item.name : item;
-        let parts = full.split('|');
-        return {
-          title: parts[0] ? parts[0].trim().substring(0, 20) : "Basliksiz",
-          content: parts[1] ? parts[1].trim() : full.trim()
-        };
-      });
+      const clean = raw.map(item => (typeof item === 'object' ? item.name : item));
       messaging.peerSocket.send(clean);
     }
   }
 }
+
 settingsStorage.onchange = sendData;
 messaging.peerSocket.onopen = sendData;
