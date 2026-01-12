f2f2323ed00e3bd35a4a3b9b5ba32a6c21a034bf
emrhky <emrhky.ek@gmail.com>
2026-01-13 00:56:43 +0300

    Update index.jsx

--- settings/index.jsx
+++ settings/index.jsx
@@ -1,13 +1,14 @@
 registerSettingsPage((props) => (
   <Page>
     <Section title="Not Listesi">
+      <Text italic>Kullanım: Başlık | Notun Kendisi</Text>
       <AdditiveList
         settingsKey="notes_list"
         maxItems="5"
         addAction={
           <TextInput
             label="Yeni Not"
-            placeholder="Notunuzu buraya yazın..."
+            placeholder="Market | Süt almayı unutma"
           />
         }
       />
