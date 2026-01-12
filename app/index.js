02d85f75c93d3c5f84338dc74b75212c869d128d
emrhky <emrhky.ek@gmail.com>
2026-01-12 03:12:38 +0300

    Update index.js

--- app/index.js
+++ app/index.js
@@ -3,18 +3,13 @@ import * as messaging from "messaging";
 
 const txt0 = document.getElementById("text-0");
 
-// Saat tarafında baglantı acıldı mı?
 messaging.peerSocket.onopen = () => {
-  txt0.text = "Baglanti Kuruldu...";
+  txt0.text = "Baglanti OK. Not Yaziniz...";
 };
 
-// Veri geldiginde
 messaging.peerSocket.onmessage = (evt) => {
-  if (evt.data && evt.data[0]) {
-    txt0.text = evt.data[0].title; // Sadece ilk notun baslıgı
+  // Gelen veri ne olursa olsun ekrana yaz
+  if (evt.data) {
+    txt0.text = evt.data.substring(0, 30); // Ekrana sığması için kısıtla
   }
-};
-
-messaging.peerSocket.onerror = (err) => {
-  txt0.text = "Hata: " + err.code;
 };
