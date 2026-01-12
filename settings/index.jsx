778cc8f9753efeafeda1287d742bd32ebf34a00e
emrhky <emrhky.ek@gmail.com>
2026-01-12 15:31:05 +0300

    Update index.jsx

--- settings/index.jsx
+++ settings/index.jsx
@@ -1,13 +1,14 @@
 registerSettingsPage((props) => (
   <Page>
     <Section title="Not Listesi">
+      <Text italic>Format: Başlık | Notun Kendisi</Text>
       <AdditiveList
         settingsKey="notes_list"
-        maxItems="10"
+        maxItems="5"
         addAction={
           <TextInput
-            label="Yeni Not Ekle"
-            placeholder="Notunuzu yazın..."
+            label="Yeni Not"
+            placeholder="Başlık | İçerik şeklinde yazın..."
           />
         }
       />
